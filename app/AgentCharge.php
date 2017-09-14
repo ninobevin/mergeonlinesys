e23a1608436d4bda52b7200d67840bfb23d5a3c9
Bevin Nino <ninobevin@gmail.com>
2017-09-14 18:47:27 +0800

    first commit

---
+++ app/AgentCharge.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AgentCharge extends Model
+{
+    //
+
+
+
+    protected $table = 'agent_charge';
+    public $timestamps = false;
+
+
+
+
+}
---
+++ app/Customer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    //
+    protected $table = 'customer';
+    public $timestamps = false;
+
+
+
+
+    public function getFullName(){
+
+
+    return	$this->fname." ".$this->mname.". ".$this->lastname." ";
+
+
+    }
+
+
+    public function address(){
+
+            
+    	 return  $this->hasOne('App\Address', 'loc_id', 'loc_id');
+
+    }
+
+
+    
+    public function transaction(){
+
+    	 return $this->hasMany('App\Transaction', 'cust_id', 'cust_id');
+
+    }
+   
+
+}
